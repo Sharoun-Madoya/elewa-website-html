f9a0e8f47b8c78e542824f35b52b52a0229f6f0c
[sharoun <[madoyasharoun@gmail.com]>
2023-10-30 14:48:31 +0300

    :hammer: displays the data

--- src/ts/index.js
+++ src/ts/index.js
@@ -1,90 +1,132 @@
 // defining the interface objects for the projectItems
 // JSDoc comment
 document.addEventListener("DOMContentLoaded", function () {
+    var slideContainer = document.querySelector(".card-container");
     var prevButton = document.querySelector(".bottom-btn1"); // Targeting the buttons
     var nextButton = document.querySelector(".bottom-btn2");
-    if (prevButton && nextButton) {
-        prevButton.addEventListener('click', function () {
-            showSlide(0);
-        });
-        nextButton.addEventListener('click', function () {
-            showSlide(3);
-        });
+    // This is the data contained in the project one as displayed on the html
+    var mockProject = [
+        {
+            title: "Introducing conversational learning",
+            description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+            tag: "Education",
+            imgSrc: "/assets/images/book2.jpg"
+        },
+        {
+            title: "Content Development for a sustainable future",
+            description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+            tag: "Environment",
+            imgSrc: "/assets/images/environment.jpg"
+        },
+        {
+            title: "Largescale training for healthcare professionals",
+            description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+            tag: "Healthcare",
+            imgSrc: "/assets/images/doctor.jpeg"
+        },
+        {
+            title: "Introducing conversational learning",
+            description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+            tag: "Education",
+            imgSrc: "/assets/images/treeMe.jpg"
+        },
+        {
+            title: "Introducing conversational learning",
+            description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+            tag: "Education",
+            imgSrc: "/assets/images/education.jpg"
+        }
+    ];
+    // Clear the slide container
+    slideContainer.innerHTML = '';
+    // Function to create a card element for each project
+    function createCard(project) {
+        var card = document.createElement('div');
+        card.className = 'card';
+        var image = document.createElement('img');
+        image.src = project.imgSrc;
+        image.alt = project.title;
+        var cardContent = document.createElement('div');
+        cardContent.className = 'card-content';
+        cardContent.innerHTML = "\n      <button>".concat(project.tag, "</button>\n      <h4>").concat(project.title, "</h4>\n      <p>").concat(project.description, "</p>\n    ");
+        card.appendChild(image);
+        card.appendChild(cardContent);
+        return card;
     }
-});
-// This is the data contained in the project one as displayed on the html
-var mockProject = [
-    {
-        title: "Introducing conversational learning",
-        description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-        tag: "Education",
-        imgSrc: "/assets/images/book2.jpg"
-    },
-    {
-        title: "Content Development for a sustainable future",
-        description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-        tag: "Environment",
-        imgSrc: "/assets/images/environment.jpg"
-    },
-    {
-        title: "Largescale training for healthcare professionals",
-        description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-        tag: "Healthcare",
-        imgSrc: "/assets/images/doctor.jpeg"
-    },
-    {
-        title: "Introducing conversational learning",
-        description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-        tag: "Education",
-        imgSrc: "/assets/images/treeMe.jpg"
-    },
-    {
-        title: "Introducing conversational learning",
-        description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-        tag: "Education",
-        imgSrc: "/assets/images/education.jpg"
+    // Append each project as a card to the slide container
+    mockProject.forEach(function (project) {
+        var card = createCard(project);
+        slideContainer.appendChild(card);
+    });
+    var slides = document.querySelectorAll('.card');
+    var index = 0;
+    function showSlide(index) {
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.opacity = '0.3';
+            slides[i].classList.remove('active');
+        }
+        slides[index].style.opacity = '1';
+        slides[index].classList.add('active');
+        if (slideContainer) {
+            slideContainer.style.transform = "translateX(-".concat(index * 100, "%)");
+        }
     }
-];
-mockProject.forEach(function (project) {
-    var image = document.getElementById('card1');
-    var imageContent = document.getElementById('card-content');
-    //image in card 
-    image.innerHTML = "<img src=".concat(project.imgSrc, " />");
-    // writtings below the image
-    imageContent.innerHTML = "<button>".concat(project.tag, "</button><h4>").concat(project.title, "</h4><p>").concat(project.description, "</p>");
-});
-var slides = document.querySelectorAll(".card"); // Targeting the projects container
-var slideContainer = document.querySelector(".card-container");
-var prevButton = document.querySelector('.bottom-btn1');
-var nextButton = document.querySelector('.bottom-btn2');
-var index = 0;
-// Get references to the elements
-function showSlide(index) {
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.opacity = '0.3';
-        slides[i].classList.remove('active');
-    }
-    slides[index].style.opacity = '1';
-    slides[index].classList.add('active');
-    if (slideContainer) {
-        slideContainer.style.transform = "translateX(-".concat(index * 10, "%)");
-    }
-}
-prevButton === null || prevButton === void 0 ? void 0 : prevButton.addEventListener('click', function () {
-    index--;
-    if (index < 0) {
-        index = slides.length - 1;
-    }
+    prevButton === null || prevButton === void 0 ? void 0 : prevButton.addEventListener('click', function () {
+        index--;
+        if (index < 0) {
+            index = slides.length - 1;
+        }
+        showSlide(index);
+    });
+    nextButton === null || nextButton === void 0 ? void 0 : nextButton.addEventListener('click', function () {
+        index++;
+        if (index >= slides.length) {
+            index = 0;
+        }
+        showSlide(index);
+    });
     showSlide(index);
 });
-nextButton === null || nextButton === void 0 ? void 0 : nextButton.addEventListener('click', function () {
-    index++;
-    if (index >= slides.length) {
-        index = 0;
-    }
-    showSlide(index);
-});
-showSlide(index);
+//   mockProject.forEach((project) =>{
+//   const image=document.getElementById('card1');
+//   const imageContent=document.getElementById('card-content')
+// //image in card 
+//   image.innerHTML = `<img src=${project.imgSrc} />`
+// // writtings below the image
+//   imageContent.innerHTML= `<button>${project.tag}</button><h4>${project.title}</h4><p>${project.description}</p>` 
+//   })
+//   const slides = document.querySelectorAll<HTMLDivElement>(".card"); // Targeting the projects container
+//   const slideContainer = document.querySelector<HTMLDivElement>(".card-container");
+//   const prevButton = document.querySelector<HTMLButtonElement>('.bottom-btn1');
+//   const nextButton = document.querySelector<HTMLButtonElement>('.bottom-btn2');
+//   let index: number = 0;
+//  // Get references to the elements
+// function showSlide(index: number) {
+//   for (let i =0; i < slides.length; i++) {
+//     slides[i].style.opacity = '0.3';
+//     slides[i].classList.remove('active');
+//   }
+//   slides[index].style.opacity = '1';
+//   slides[index].classList.add('active');
+//   if (slideContainer) {
+//     slideContainer.style.transform = `translateX(-${index * 10}%)`;
+//   }
+//   }
+// prevButton?.addEventListener('click', () => {
+//   index--;
+//   if (index < 0) {
+//     index = slides.length - 1;
+//   }
+//   showSlide(index);
+// });
+// nextButton?.addEventListener('click', () => {
+//   index++;
+//   if (index >= slides.length) {
+//     index = 0;
+//   }
+//   showSlide(index);
+// });
+// showSlide(index);
 // const projectOne: ProjectItem = {
 //   title: "Introducing conversational learning",
 //   description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,118 +1,174 @@
 // defining the interface objects for the projectItems
 // JSDoc comment
 document.addEventListener("DOMContentLoaded", () =>{
+  const slideContainer = document.querySelector<HTMLDivElement>(".card-container");
   const prevButton = document.querySelector<HTMLButtonElement>(".bottom-btn1"); // Targeting the buttons
   const nextButton = document.querySelector<HTMLButtonElement>(".bottom-btn2");
 
-  if (prevButton && nextButton) {
-    prevButton.addEventListener('click', () => {
-      showSlide(0);
-    });
-
-    nextButton.addEventListener('click', () => {
-      showSlide(3);
-    });
-
-  }})
-
-/**
+  /**
  * Interface for classes that represent a project.
  */
 interface ProjectItem {
-    title: string;
-    description: string;
-    tag: string;
-    imgSrc: string;
-  }
-  
-  // This is the data contained in the project one as displayed on the html
-  
-  const mockProject:ProjectItem[]=[
-  {
+  title: string;
+  description: string;
+  tag: string;
+  imgSrc: string;
+}
+
+// This is the data contained in the project one as displayed on the html
+
+const mockProject:ProjectItem[]=[
+{
+  title: "Introducing conversational learning",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Education",
+  imgSrc: "/assets/images/book2.jpg"
+},
+{
+  title: "Content Development for a sustainable future",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Environment",
+  imgSrc: "/assets/images/environment.jpg"
+},
+{
+  title: "Largescale training for healthcare professionals",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Healthcare",
+  imgSrc: "/assets/images/doctor.jpeg"
+},
+{
+  title: "Introducing conversational learning",
+  description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
+  tag: "Education",
+  imgSrc: "/assets/images/treeMe.jpg"
+},
+{
     title: "Introducing conversational learning",
     description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
     tag: "Education",
-    imgSrc: "/assets/images/book2.jpg"
-  },
- {
-    title: "Content Development for a sustainable future",
-    description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-    tag: "Environment",
-    imgSrc: "/assets/images/environment.jpg"
-  },
-  {
-    title: "Largescale training for healthcare professionals",
-    description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-    tag: "Healthcare",
-    imgSrc: "/assets/images/doctor.jpeg"
-  },
-  {
-    title: "Introducing conversational learning",
-    description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-    tag: "Education",
-    imgSrc: "/assets/images/treeMe.jpg"
-  },
-  {
-      title: "Introducing conversational learning",
-      description: "Eos qui ratione voluptatem sequi nesciunt.Neque porro quisquam dolorem ipsum quia",
-      tag: "Education",
-      imgSrc: "/assets/images/education.jpg"
-    }];
-  
+    imgSrc: "/assets/images/education.jpg"
+  }];
 
-  mockProject.forEach((project) =>{
-  const image=document.getElementById('card1');
-  const imageContent=document.getElementById('card-content')
-//image in card 
-  image.innerHTML = `<img src=${project.imgSrc} />`
-// writtings below the image
-  imageContent.innerHTML= `<button>${project.tag}</button><h4>${project.title}</h4><p>${project.description}</p>` 
-  })
 
-  const slides = document.querySelectorAll<HTMLDivElement>(".card"); // Targeting the projects container
-  const slideContainer = document.querySelector<HTMLDivElement>(".card-container");
-  const prevButton = document.querySelector<HTMLButtonElement>('.bottom-btn1');
-  const nextButton = document.querySelector<HTMLButtonElement>('.bottom-btn2');
+    // Clear the slide container
+  slideContainer.innerHTML = '';
 
-  let index: number = 0;
- // Get references to the elements
-function showSlide(index: number) {
-  for (let i =0; i < slides.length; i++) {
-    slides[i].style.opacity = '0.3';
-    slides[i].classList.remove('active');
-  }
-  slides[index].style.opacity = '1';
-  slides[index].classList.add('active');
-  if (slideContainer) {
-    slideContainer.style.transform = `translateX(-${index * 10}%)`;
-  }
+
+  // Function to create a card element for each project
+  function createCard(project: ProjectItem) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const image = document.createElement('img');
+    image.src = project.imgSrc;
+    image.alt = project.title;
+
+    const cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
+    cardContent.innerHTML = `
+      <button>${project.tag}</button>
+      <h4>${project.title}</h4>
+      <p>${project.description}</p>
+    `;
+
+    card.appendChild(image);
+    card.appendChild(cardContent);
+
+    return card;
   }
 
+  // Append each project as a card to the slide container
+  mockProject.forEach(project => {
+    const card = createCard(project);
+    slideContainer.appendChild(card);
+  });
 
-prevButton?.addEventListener('click', () => {
-  index--;
-  if (index < 0) {
-    index = slides.length - 1;
+  const slides = document.querySelectorAll<HTMLDivElement>('.card');
+  let index = 0;
+
+  function showSlide(index: number) {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.opacity = '0.3';
+      slides[i].classList.remove('active');
+    }
+    slides[index].style.opacity = '1';
+    slides[index].classList.add('active');
+    if (slideContainer) {
+      slideContainer.style.transform = `translateX(-${index * 100}%)`;
+    }
   }
+
+  prevButton?.addEventListener('click', () => {
+    index--;
+    if (index < 0) {
+      index = slides.length - 1;
+    }
+    showSlide(index);
+  });
+
+  nextButton?.addEventListener('click', () => {
+    index++;
+    if (index >= slides.length) {
+      index = 0;
+    }
+    showSlide(index);
+  });
+
   showSlide(index);
 });
 
-nextButton?.addEventListener('click', () => {
-  index++;
-  if (index >= slides.length) {
-    index = 0;
-  }
-  showSlide(index);
-});
-
-showSlide(index);
 
 
 
 
 
 
+//   mockProject.forEach((project) =>{
+//   const image=document.getElementById('card1');
+//   const imageContent=document.getElementById('card-content')
+// //image in card 
+//   image.innerHTML = `<img src=${project.imgSrc} />`
+// // writtings below the image
+//   imageContent.innerHTML= `<button>${project.tag}</button><h4>${project.title}</h4><p>${project.description}</p>` 
+//   })
 
+//   const slides = document.querySelectorAll<HTMLDivElement>(".card"); // Targeting the projects container
+//   const slideContainer = document.querySelector<HTMLDivElement>(".card-container");
+//   const prevButton = document.querySelector<HTMLButtonElement>('.bottom-btn1');
+//   const nextButton = document.querySelector<HTMLButtonElement>('.bottom-btn2');
+
+//   let index: number = 0;
+//  // Get references to the elements
+// function showSlide(index: number) {
+//   for (let i =0; i < slides.length; i++) {
+//     slides[i].style.opacity = '0.3';
+//     slides[i].classList.remove('active');
+//   }
+//   slides[index].style.opacity = '1';
+//   slides[index].classList.add('active');
+//   if (slideContainer) {
+//     slideContainer.style.transform = `translateX(-${index * 10}%)`;
+//   }
+//   }
+
+
+// prevButton?.addEventListener('click', () => {
+//   index--;
+//   if (index < 0) {
+//     index = slides.length - 1;
+//   }
+//   showSlide(index);
+// });
+
+// nextButton?.addEventListener('click', () => {
+//   index++;
+//   if (index >= slides.length) {
+//     index = 0;
+//   }
+//   showSlide(index);
+// });
+
+// showSlide(index);
 
 
 
